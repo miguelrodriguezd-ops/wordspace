c5f7bf8129c8e7aa8376585b26752ccb2bb923a4
miguelrodriguezd-ops <miguelrodriguezd@ielacandelariamedellin.edu.co>
2026-04-21 21:10:26 +0000

    21-04

---
+++ 21-04/ejercicio5.5.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0) {
+        continue;
+    }
+    if (i === 17) {
+        break;
+    }
+    console.log(i);
+}
---
+++ 21-04/ejercicio5.8.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 5; i++) {
+    let linea = '';
+    for (let j = 1; j <= i; j++) {
+        linea += '* ';
+    }
+    console.log(linea.trim());
+}
